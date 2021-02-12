e39670965e63605378f933da5aad1f1226e0461d
Sugai-Ayano <b3w41067a@gmail.com>
2021-02-12 11:36:43 +0000

    deploy setting

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,6 +18,7 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
 $(function () {
     $('#user_postal_code').jpostal({
       postcode: [
@@ -29,4 +30,29 @@ $(function () {
         "#user_street": "%6%7"
       }
     });
+});
+
+$(document).ready(function () {
+  $("#theTarget").skippr({
+    // スライドショーの変化 ("fade" or "slide")
+    transition : 'slide',
+    // 変化に係る時間(ミリ秒)
+    speed : 1000,
+    // easingの種類
+    easing : 'easeOutQuart',
+    // ナビゲーションの形("block" or "bubble")
+    navType : 'block',
+    // 子要素の種類('div' or 'img')
+    childrenElementType : 'div',
+    // ナビゲーション矢印の表示(trueで表示)
+    arrows : true,
+    // スライドショーの自動再生(falseで自動再生なし)
+    autoPlay : true,
+    // 自動再生時のスライド切替間隔(ミリ秒)
+    autoPlayDuration : 3000,
+    // キーボードの矢印キーによるスライド送りの設定(trueで有効)
+    keyboardOnAlways : true,
+    // 一枚目のスライド表示時に戻る矢印を表示するかどうか(falseで非表示)
+    hidePrevious : false
+  });
 });
