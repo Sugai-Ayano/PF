014c94daf4c69a08265aba4d341c00edb7a05386
Sugai-Ayano <b3w41067a@gmail.com>
2021-02-16 15:27:01 +0000

    posts/index季節ページ同期処理完了

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,15 +3,17 @@ class PostsController < ApplicationController
   before_action :ensure_correct_user, only:[:edit, :update, :destroy]
 
   def index
+    @all_ranks = Post.find(Favorite.group(:post_id).order('count(post_id) desc').limit(3).pluck(:post_id))
     if params[:season] == nil
       @post_all = Post.all
       @post_all = Post.page(params[:page]).per(9)
       @post = Post.new
       # いいね順に上位３つの投稿を表示
       # byebug
-      @all_ranks = Post.find(Favorite.group(:post_id).order('count(post_id) desc').limit(3).pluck(:post_id))
+      # @all_ranks = Post.find(Favorite.group(:post_id).order('count(post_id) desc').limit(3).pluck(:post_id))
     else
-      @posts = Post.where(genre_id: Post.genre_ids[params[:season]]).page(params[:page]).per(9)
+      # @posts = Post.where(genre_id: Post.genre_ids[params[:season]]).page(params[:page]).per(9)
+      @posts = Post.where(genre_id: Post.genre_ids[params[:season]])
       @genre_name = params[:season]
     end
   end
