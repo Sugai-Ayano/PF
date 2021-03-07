9df46391e7c69971a4ded6fad4d597eacb01a837
Sugai-Ayano <b3w41067a@gmail.com>
2021-03-07 07:06:29 +0000

    [Add]homes_controllerゲストユーザー名前追加

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -11,6 +11,7 @@ class HomesController < ApplicationController
   def new_guest
     user = User.find_or_create_by!(email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
+      user.name = 'ゲストユーザー'
       #user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
     end
     sign_in user
